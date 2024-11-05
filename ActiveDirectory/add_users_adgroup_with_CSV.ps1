c73366b4fb795785b93164ea051700c71fd5dec6
qlainay <84079113+qlainay@users.noreply.github.com>
2024-11-05 11:23:29 +0100

    Add files via upload

---
+++ ActiveDirectory/add_users_adgroup_with_CSV.ps1
@@ -0,0 +1,8 @@
+﻿Import-Module ActiveDirectory
+$Serveur = "controlleur.de.domaineFQDN"
+$Groupe  = "nom_du_groupeE"
+$list_user = get-content "C:\chemin\adduseradgroup.csv"
+
+foreach ($user in $list_user){
+    Add-ADGroupMember -Server $Serveur -Identity $Groupe -Confirm:$false
+}
